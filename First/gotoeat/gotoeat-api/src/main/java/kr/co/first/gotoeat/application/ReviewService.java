9f1aacc9116acc3ab5889664fac16af31270c50b
uHan2 <asgd56@gmail.com>
2020-02-17 14:36:27 +0900

    Add Review Service

---
+++ First/gotoeat/gotoeat-api/src/main/java/kr/co/first/gotoeat/application/ReviewService.java
@@ -0,0 +1,13 @@
+package kr.co.first.gotoeat.application;
+
+import kr.co.first.gotoeat.domain.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService
+{
+    public void addReview(Review review)
+    {
+
+    }
+}
---
+++ First/gotoeat/gotoeat-api/src/main/java/kr/co/first/gotoeat/domain/Review.java
@@ -0,0 +1,20 @@
+package kr.co.first.gotoeat.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review
+{
+    @Id
+    @GeneratedValue
+    private long id;
+}
