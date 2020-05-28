94270f9ba1c8b8e2dc79457a3e762170e76c9cb2
uHan2 <asgd56@gmail.com>
2020-05-28 19:15:46 +0900

    Try again

---
+++ first/eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/application/MenuItemService.java
@@ -0,0 +1,16 @@
+package kr.co.fastcampus.eatgo.application;
+
+import kr.co.fastcampus.eatgo.domain.MenuItem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService
+{
+
+    public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems)
+    {
+        //toDo : update
+    }
+}
