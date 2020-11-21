4ca4bcd19219d2baa552c86a8d73e92fae41426e
uHan <asgd56@gmail.com>
2020-11-21 21:32:06 +0900

    new Spring Boot Example

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -41,7 +41,7 @@ public class Order
     }
 
     //연관 관계 편의 메소드
-    public void setOrderItem(OrderItem orderItem)
+    public void addOrderItem(OrderItem orderItem)
     {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
@@ -52,5 +52,56 @@ public class Order
     {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems)
+    {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems)
+        {
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    //비즈니스 로직
+
+    /**
+     * 주문취소
+     */
+    public void cancel()
+    {
+        if(delivery.getStatus() == DeliveryStatus.COMP)
+        {
+            throw new IllegalStateException("이미 배송이 완료되었습니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+
+        for(OrderItem orderItem : orderItems)
+        {
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice()
+    {
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems)
+        {
+            totalPrice += orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
     }
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
@@ -23,5 +24,30 @@ public class OrderItem
     private Order order;
 
     private int orderPrice; //주문 당시 가격
-    private int count; // 주문 당시 수
+    private int count; // 주문 수량
+
+    //생성 메소드//
+    public static OrderItem createOrderItems(Item item, int orderPrice, int count)
+    {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    //비즈니스 로직
+    public void cancel()
+    {
+        getItem().addStock(count);
+    }
+
+    //조회 로직
+    public int getTotalPrice()
+    {
+        return getOrderPrice() * getCount();
+    }
 }
