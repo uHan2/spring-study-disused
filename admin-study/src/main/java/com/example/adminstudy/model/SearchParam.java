ad34467a66ee37ac6c970ab55be4df01a76098f3
uHan <asgd56@gmail.com>
2021-02-08 13:49:32 +0900

    connect MySQL db

--- admin-study/src/main/java/com/example/adminstudy/model/SearchParam.java
+++ admin-study/src/main/java/com/example/adminstudy/model/SearchParam.java
@@ -1,10 +1,9 @@
 package com.example.adminstudy.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter @Setter
+@Data
 @AllArgsConstructor
 public class SearchParam
 {
