8b4926724478b598d1ea8d7234b8319cb1add838
uHan <asgd56@gmail.com>
2021-02-08 09:52:44 +0900

    get members api

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +19,49 @@ public class MemberApiController
 {
     private final MemberService memberService;
 
+
+    /**
+     * 이렇게 엔티티를 직접 노출 하면 안된다 ..
+     *
+     * @return
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1()
+    {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2()
+    {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>
+    {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto
+    {
+        private String name;
+    }
+
+    /**
+     * 이렇게 엔티티를 바로 받으면 안됨!
+     *
+     * @param member
+     * @return
+     */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member)
     {
