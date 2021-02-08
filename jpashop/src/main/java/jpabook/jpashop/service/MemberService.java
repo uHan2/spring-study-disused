56a9827063cfcb841c86ba03709da3af378707e5
uHan <asgd56@gmail.com>
2021-02-08 09:38:21 +0900

    add update api

--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -51,4 +51,10 @@ public class MemberService
         return memberRepository.findOne(memberId);
     }
 
+    @Transactional
+    public void update(Long id, String name)
+    {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
