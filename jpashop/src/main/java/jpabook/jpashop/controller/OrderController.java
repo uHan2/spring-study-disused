f1d9d330c340b7dda3c61c08abd77c602a90bca7
uhan <asgd56@gmail.com>
2021-02-09 16:20:59 +0900

    add another way binding data to variable (command object)

--- jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -7,6 +7,8 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -34,14 +36,29 @@ public class OrderController
         return"order/orderForm";
     }
 
+    /**
+     * @RequestParam 으로 요청값을 변수에 바인딩해도 되고, OrderRequestDto 같은 Command Object를 통해 바인딩 해도 된다.
+     */
     @PostMapping("/order")
-    public String order(@RequestParam("memberId") Long memberId,
-                        @RequestParam("itemId") Long itemId,
-                        @RequestParam("count") int count)
+    public String order(OrderRequestDto orderRequestDto)
+//    public String order(@RequestParam("memberId") Long memberId,
+//                        @RequestParam("itemId") Long itemId,
+//                        @RequestParam("count") int count)
     {
-        orderService.order(memberId, itemId, count);
+//        orderService.order(memberId, itemId, count);
+//        orderService.order(orderRequestDto); // 이렇게 command object 를 넘겨도 되고 (대신 구현해야함)
+        orderService.order(orderRequestDto.getMemberId(), orderRequestDto.getItemId(), orderRequestDto.getCount());
 
         return "redirect:/orders";
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class OrderRequestDto
+    {
+        private Long memberId;
+        private Long itemId;
+        private int count;
     }
 
     @GetMapping("/orders")
